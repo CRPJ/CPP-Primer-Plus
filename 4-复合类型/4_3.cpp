650baef1b0bf85823e278eb8c46d32e95f71d04f
Heng Wang <wh9697yyk@163.com>
2019-04-03 22:48:10 +0800

    Update 4_3.cpp

--- 4-复合类型/4_3.cpp
+++ 4-复合类型/4_3.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cstring>
 
+int main()
+{
+    using namespace std;
+    char first_name[80];
+    char last_name[20];
+    cout << "Enter your first name: ";
+    cin.getline(first_name, 20);
+    cout << "Enter your last name: ";
+    cin.getline(last_name, 20);
+    strcat(first_name, ", ");
+    strcat(first_name, last_name);
+
+    cout << "Here's the information in a single string: " << first_name;
+
+    return 0;
+}
