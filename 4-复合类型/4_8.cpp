fdf0648b6543042b4c314e54a96e29b34a1bdf89
Heng Wang <wh9697yyk@163.com>
2019-04-04 21:56:57 +0800

    Update 4_8.cpp

--- 4-复合类型/4_8.cpp
+++ 4-复合类型/4_8.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+
+struct pizza
+{
+    char name[20];
+    float diameter;
+    float weight;
+};
+
+int main()
+{
+    using namespace std;
+
+    pizza * example = new pizza;
+    cout << "Enter pizza's diameter before enter its name: ";
+    cin >> example->diameter;
+    cout << "Enter pizza's name: ";
+    cin >> (*example).name;
+    cout << "Enter pizza's weight: ";
+    cin >> (*example).weight;
+
+    cout << "Pizza's name is: " << example->name;
+    cout << "Pizza's diameter is: " << (*example).diameter;
+    cout << "Pizza's weight is: " << (*example).weight;
+
+    return 0;
+}
